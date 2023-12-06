5fe83d12c48cc47263bc3ce497dbe50dce84181c
barbaravazferreira <barbara01052002@gmail.com>
2023-12-06 14:00:00 -0300

    adição de funcionalidaades importantes

--- logica.js
+++ logica.js
@@ -1,34 +1,66 @@
-var lista = window.document.getElementById('listaTarefas')
-var tarefas = []
+var lista = window.document.getElementById('listaTarefas');
+var tarefas = [];
 
-function exibirLista(tarefa) {
+function exibirLista(tarefa, feita) {
     var novoItem = window.document.createElement('li');
     novoItem.innerHTML = `
     <ul class="itemTarefas">
-        <input class="feito" type="checkbox"> 
+        <input class="feito" type="checkbox" ${feita ? 'checked' : ''} onchange="atualizarEstado('${tarefa}', this.checked)"> 
         <p class="textoTarefa">${tarefa}</p>
         <button class="buttonExcluir" onclick="apagarTarefa('${tarefa}')"><img class="lixo" src="https://cdn-icons-png.flaticon.com/512/126/126468.png"></button>
     </ul>
-    `
+    `;
     lista.appendChild(novoItem);
 }
 
 function inserirTarefa() {
     var tarefa = window.document.getElementById('textoInserir').value;
-    tarefas.push(tarefa);
-    exibirLista(tarefa);
-    window.document.getElementById('textoInserir').value = ''
+    tarefas.push({ descricao: tarefa, feita: false });
+    atualizarLista();
+    window.document.getElementById('textoInserir').value = '';
+}
+
+function atualizarEstado(tarefa, estado) {
+    tarefas.forEach(function (item) {
+        if (item.descricao === tarefa) {
+            item.feita = estado;
+        }
+    });
+    atualizarLista();
 }
 
 function atualizarLista() {
     lista.innerHTML = '';
-    tarefas.forEach(exibirLista);
+    var mostrarFeitas = document.getElementById('feitas').classList.contains('selecionado');
+
+    tarefas.forEach(function (tarefa) {
+        if (mostrarFeitas && tarefa.feita) {
+            exibirLista(tarefa.descricao, true);
+        } else if (!mostrarFeitas && !tarefa.feita) {
+            exibirLista(tarefa.descricao, false);
+        }
+    });
+
+    var btnFazer = document.getElementById('aFazer');
+    var btnFeitas = document.getElementById('feitas');
+
 }
 
 function apagarTarefa(tarefa) {
-    var indiceTarefa = tarefas.indexOf(tarefa);
-    if (indiceTarefa !== -1) {
-        tarefas.splice(indiceTarefa, 1);
-        atualizarLista();
-    }
+    tarefas = tarefas.filter(function (item) {
+        return item.descricao !== tarefa;
+    });
+    atualizarLista();
 }
+
+document.getElementById('aFazer').addEventListener('click', function () {
+    document.getElementById('aFazer').classList.add('selecionado');
+    document.getElementById('feitas').classList.remove('selecionado');
+    atualizarLista();
+});
+
+document.getElementById('feitas').addEventListener('click', function () {
+    document.getElementById('feitas').classList.add('selecionado');
+    document.getElementById('aFazer').classList.remove('selecionado');
+    atualizarLista();
+});
